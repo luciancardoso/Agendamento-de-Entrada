f1fe6c731e2501732f4985cad10037ed2444991b
Lucian Cardoso <luciancardoso20@gmail.com>
2021-11-18 14:24:05 -0400

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'lucian.santos/sistema-aduana',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'lucian.santos/sistema-aduana' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
