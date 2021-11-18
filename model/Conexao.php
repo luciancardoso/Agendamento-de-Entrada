1576879b06f79593325ce31811f155f2199a0f9c
Lucian Cardoso <luciancardoso20@gmail.com>
2021-11-18 14:23:17 -0400

    Add files via upload

---
+++ model/Conexao.php
@@ -0,0 +1,16 @@
+<?php
+
+class Conexao {
+
+    public static $instance;
+
+    public static function get_instance(){
+        if(!isset(self::$instance)){
+            self::$instance = new PDO('mysql:host=localhost;dbname=testeweb','root','',array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+            self::$instance->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        }
+        return self::$instance;
+
+    }
+
+}
